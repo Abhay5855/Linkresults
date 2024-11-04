ec1d444adff88e90e31e0379ca24e608adebd208
Abhishek <abhishekbulbule384@gmail.com>
2024-11-04 07:27:34 +0530

    Refactor(Signup Page) - Changed Logo and some texts

--- src/components/signup/SignupSideBanner.tsx
+++ src/components/signup/SignupSideBanner.tsx
@@ -12,9 +12,9 @@ const SignupSideBanner:FC = () => {
         </p>
         <div className="flex flex-col justify-center items-center gap-1 p-0 m-0">
           <img src={image} alt="face" height={80} width={80} loading="lazy" />
-          <p className="font-semibold">Justin bhai</p>
+          <p className="font-semibold">Justin Ozanich</p>
           <p className="font-normal">Marketing Manager</p>
-          <p className="font-normal">Chai Coffee Wala</p>
+          <p className="font-normal">Foster Coffee Company</p>
         </div>
       </div>
     </div>
--- src/pages/sigup-page/index.tsx
+++ src/pages/sigup-page/index.tsx
@@ -1,21 +1,20 @@
 import { FC } from "react";
-import imagecopy from "../../assets/images/image copy.png";
 import SignupSideBanner from "../../components/signup/SignupSideBanner";
 import MainContainer from "../../components/signup/MainContainer";
+import Logo from "../../components/base/logo/Logo";
 
 const SignUp: FC = () => {
-  
   return (
     <div className="flex flex-row min-h-screen">
       <div className="flex-grow w-[66%] h-screen overflow-y-auto  flex flex-col">
-        <div className="w-full mt-10 px-14 pb-5">
-          <img src={imagecopy} alt="logo" height={24} width={112} />
+        <div className="w-full mt-10 px-8 sm:px-14 pb-5">
+          <Logo/>
         </div>
         {/* Form Section */}
-        <MainContainer/>
+        <MainContainer />
       </div>
       {/* sidebanner section  */}
-      <SignupSideBanner/>
+      <SignupSideBanner />
     </div>
   );
 };
